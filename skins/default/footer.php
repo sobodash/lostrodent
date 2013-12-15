459a0a09747b375336126830b65adb10854d0661
Derrick Sobodash <d@arubus.(none)>
2013-12-16 07:49:14 +0800

    Pulling dead files.

--- skins/default/footer.php
+++
@@ -1,9 +0,0 @@
-			<p id="footer">
-				<a href="http://validator.w3.org/check?uri=referer" id="valid"><img src="http://www.w3.org/Icons/valid-html32-blue" height="31" width="88"></a>
-				<i>Powered by <a href="https://github.com/sobodash/lostrodent/">The Lost Rodent</a> <?php echo LR_VERSION; ?>. Released under the <a href="http://opensource.org/licenses/bsd-license.php">BSD license</a>.
-				<br>
-				Copyright &copy; 2011, 2012, 2013 Derrick Sobodash.</i>
-			</p>
-		</div>
-	</body>
-</html>
